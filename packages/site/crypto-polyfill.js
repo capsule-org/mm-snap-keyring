55df7ecf7c3f570da7f21e4255492ce67a883750
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-01-18 10:41:59 -0800

    readme cleanup of no irrelevant instructions (#4)

--- packages/site/crypto-polyfill.js
+++ packages/site/crypto-polyfill.js
@@ -1,14 +1,7 @@
-// Import the necessary modules
 const crypto = require('crypto-browserify');
-// const additionalWebCrypto = require('some-webcrypto-polyfill'); // Replace with actual module
 
-// Implement the webcrypto shim
-const webcrypto = {
-    // Your implementation here
-    // Use functionality from crypto and additionalWebCrypto
-};
+const webcrypto = {};
 
-// Combine and export
 module.exports = {
     ...crypto,
     webcrypto: webcrypto,
